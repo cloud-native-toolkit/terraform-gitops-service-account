9d8f3a69af5ab418591e13d1d5fb77a47cea225a
Sean Sundberg <seansund@us.ibm.com>
2021-07-14 23:45:57 -0500

    Sets sensitive flag
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource null_resource setup_gitops {
     command = "${path.module}/scripts/setup-gitops.sh '${var.name}' '${local.yaml_dir}' 'namespace/${var.namespace}' '${local.application_branch}' '${var.namespace}'"
 
     environment = {
-      GIT_CREDENTIALS = jsonencode(nonsensitive(var.git_credentials))
+      GIT_CREDENTIALS = jsonencode(var.git_credentials)
       GITOPS_CONFIG = jsonencode(local.layer_config)
     }
   }
