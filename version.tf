a459d0b60c92d885bb0ce02eb1dc5f4f3e114e70
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-02-10 16:02:57 -0600

    Converts to use gitops provider
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- version.tf
+++ version.tf
@@ -2,4 +2,11 @@ terraform {
   required_version = ">= 0.15.0"
 
   experiments = [module_variable_optional_attrs]
+
+  required_providers {
+    gitops = {
+      source = "cloud-native-toolkit/gitops"
+      version = ">= 0.1.7"
+    }
+  }
 }
